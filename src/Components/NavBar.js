a1c29a189b07258307b9769448015a53ec606c23
Jesus Molina <netod90@gmail.com>
2023-05-15 16:01:41 -0700

    Add active links & minimal styling to NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,17 @@
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../styles/navbar.css';
+import logo from '../media/planet.png';
 
 const Navbar = () => (
-  <nav>
-    <div>
-      <Link to="/" activeClassName="active-link link">Rockets</Link>
-      <NavLink to="/Missions" className="link">Missions</NavLink>
-      <NavLink to="/MyProfile" className="link">My Profile</NavLink>
+  <nav className="nav-container">
+    <div className="logo-container">
+      <img src={logo} className="logo" alt="logo" />
+      Space Travelers&apos; Hub
+    </div>
+    <div className="links-container">
+      <NavLink to="/Rockets" className={({ isActive }) => (isActive ? 'link-active' : 'link')}>Rockets</NavLink>
+      <NavLink to="/Missions" className={({ isActive }) => (isActive ? 'link-active line' : 'link line')}>Missions</NavLink>
+      <NavLink to="/MyProfile" className={({ isActive }) => (isActive ? 'link-active' : 'link')}>My Profile</NavLink>
     </div>
   </nav>
 );
